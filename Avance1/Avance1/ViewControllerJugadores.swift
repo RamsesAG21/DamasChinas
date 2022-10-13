c375c880bb0baafa2b4affc630544148575c5da3
José Luis flores <jlflores1999@gmail.com>
2022-10-12 23:30:05 -0500

    Archivos base
    Se agregaron los archivos base para la manipulacion de datos entre vistas

---
+++ Avance1/Avance1/ViewControllerJugadores.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerJugadores.swift
+//  Avance1
+//
+//  Created by user223065 on 10/12/22.
+//
+
+import UIKit
+
+class ViewControllerJugadores: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Avance1/Avance1/ViewControllerTutorial.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerTutorial.swift
+//  Avance1
+//
+//  Created by user223065 on 10/12/22.
+//
+
+import UIKit
+
+class ViewControllerTutorial: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
